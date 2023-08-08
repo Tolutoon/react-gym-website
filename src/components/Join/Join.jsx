37a32be388715a9fefb80c26fb1dd812bed424ec
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-08-08 20:49:34 +0100

    finalized

---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Join.css';
+
+const Join = () => {
+  return (
+    <div className='Join' id='join-us'>
+        <div className='left-j'>
+            <hr/>
+            <div>
+                <span className='stroke'>READY TO</span>
+                <span> LEVEL UP</span>
+            </div>
+            <div>
+                <span>YOUR BODY</span>
+                <span className='stroke'> WITH US?</span>
+            </div>
+        </div>
+        <div className='right-j'></div>
+    </div>
+  )
+}
+
+export default Join;
